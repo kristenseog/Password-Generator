35370507be4266aefa4d5a7135a8d11626a441f4
kristenseog <seog.kristen.j@gmail.com>
2022-12-15 13:09:14 -0500

    password gen upload

---
+++ script.js
@@ -0,0 +1,210 @@
+
+//code that generates two random 15 length password when user clicks the button
+//set up ability to set the length variation
+//add "copy-on-click" button
+//Toggle "symbols" and " numbers" on/off switch/control
+
+
+const upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateEL = document.getElementById("generate-el") //generate button
+// let versionOneEl = document.getElementById("versionOne-el" )
+// let versionTwoEl = document.getElementById("versionTwo-el")
+
+const pwElV1 = document.getElementById("pwV1");
+const pwElV2 = document.getElementById("pwV2");
+
+const copyElV1 = document.getElementById("copyV1"); //need to break into two version copy 1 & 2
+const copyElV2 = document.getElementById("copyV2"); //need to break into two version copy 1 & 2
+
+const lengthEl = document.getElementById("length");
+const upperEl = document.getElementById("UpperCase");
+const lowerEl =  document.getElementById("LowerCase");
+const numberEl = document.getElementById("Numbers");
+const symbolEl = document.getElementById("Symbols");
+
+//using scrimba tips
+// let passwordLength = 15
+    //create a randomCharacterGen() //this is going to generate a single random character in our array
+    // function randomCharacterGen() {
+    //     let randomCharacter = Math.floor(Math.random() * characters.length)
+    //     return characters[randomCharacter]
+    // } NO LONGER WORKS BC BROKEN INTO 4 CONSTS = NEED NEW FXNS FOR EACH
+    //then, create a passwordGen() with 15 length 
+    function randomUpperCaseGen() {
+        let randomUpperCase = Math.floor(Math.random() * upperLetters.length)
+        return upperLetters[randomUpperCase]
+    }
+
+    function randomLowerCaseGen() {
+        let randomLowerCase = Math.floor(Math.random() * lowerLetters.length)
+        return lowerLetters[randomLowerCase]
+    }
+
+    function randomNumbersGen() {
+        let randomNumbers = Math.floor(Math.random() * numbers.length)
+        return numbers[randomNumbers]
+    }
+
+    function randomSymbolsGen() {
+        let randomSymbols = Math.floor(Math.random() * symbols.length)
+        return symbols[randomSymbols]
+    }
+
+function passwordGen() {
+    const length = lengthEl.value; 
+
+    let randomPassword = ""; //create a string to be filled in
+
+    if (upperEl.checked) {
+        randomPassword += randomUpperCaseGen()
+    }
+    if (lowerEl.checked) {
+        randomPassword += randomLowerCaseGen()
+    }
+    if (numberEl.checked) {
+        randomPassword += randomNumbersGen()
+    }
+    if (symbolEl.checked) {
+        randomPassword += randomSymbolsGen()
+    }
+
+
+    for (let i = randomPassword.length; i < length; i++) {
+        const X = generateX()
+        randomPassword += X;
+        }       
+
+        pwElV1.textContent = randomPassword; //not working? after adding 2nd 
+        pwElV2.textContent = randomPassword;
+// console.log(randomPassword)
+// pwElV1.textContent = randomPassword
+}
+
+// how to i set it to generate two at a time??????
+// let passwordGenOne = passwordGen();
+// let passwordGenTwo = passwordGen();
+// function generateVersion() {
+// pwElV1.textContent = passwordGenOne;
+// pwElV2.textContent = passwordGenTwo;
+// }
+// pwElV1.textContent = randomPassword();
+// pwElV2.textContent = randomPassword();
+
+
+// let passwordGenOne = randomPassword()
+// let passwordGenTwo = randomPassword()
+
+
+
+// function generateVersion() {
+//     versionOneEl.textContent = "Version 1: " + passwordGenOne
+//     versionTwoEl.textContent = "Version 2: " + passwordGenTwo    
+// }
+
+
+    function generateX() {
+        const Xs = [];
+        
+        if (upperEl.checked) {
+            Xs.push(randomUpperCaseGen());
+        }
+        if (lowerEl.checked) {
+            Xs.push(randomLowerCaseGen());
+        }
+        if (numberEl.checked) {
+            Xs.push(randomNumbersGen());
+        }
+        if (symbolEl.checked) {
+            Xs.push(randomSymbolsGen());
+        }
+       
+        if (Xs.length === 0) //what does this mean?
+            return ""
+            // return "check at least one box to generate "
+            return Xs[Math.floor(Math.random() * Xs.length)];
+
+    }
+
+
+    
+// function generateVersion() {
+//     pwElV1.textContent = "";
+//     pwElV2.textContent = "";
+
+//     pwElV1.textContent += passwordGen();
+//     pwElV2.textContent += passwordGen();
+// }
+    
+    copyElV1.addEventListener("click", () => {
+        const textarea = document.createElement("textarea");
+        const randomPassword = pwElV1.textContent;
+        
+        if(!randomPassword) { //what is this???
+            return;
+        }
+
+        textarea.value = randomPassword;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copyV1");
+        textarea.remove();
+        alert("randomPassword copied to clipboard");
+    })
+
+    copyElV2.addEventListener("click", () => {
+        const textarea = document.createElement("textarea");
+        const randomPassword = pwElV2.textContent;
+        
+        if(!randomPassword) { //what is this???
+            return;
+        }
+
+        textarea.value = randomPassword;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copyV2");
+        textarea.remove();
+        alert("randomPassword copied to clipboard");
+    })
+
+
+
+
+    function generateVersion() {
+        pwElV1.textContent = "";
+        pwElV2.textContent = "";
+    
+        pwElV1.textContent += passwordGen();
+        pwElV2.textContent += passwordGen();
+
+
+        //create an alert message here if none of the boxes are checked off**
+    }
+
+
+
+
+
+
+    //generateEl.addEventListener("click", passwordGen) or setup on html button onclick
+//     generateEl.addEventListener("click", () => {
+//         if(!randomPassword) {
+//             return "check at least one box to generate a password";
+//         }
+//     }
+//    )   //  else if (alert("check at least one box to generate a password")) {
+    //         //     let message = alert("check at least one box to generate a password");
+    //         // return message;
+    //             if (!alert("check at least one box to generate a password")) {
+    //         window.location.reload();
+    //             }
+    //          }
+    //setTimeout(function() {notification.close()}, 1000)
+//     var alert = bootbox.alert('Massage')
+// alert.show();
+// setTimeout(function(){alert.modal('hide'); }, 4000);
